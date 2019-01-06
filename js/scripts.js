25e43f7d325c3a1557bae20527e9de8307638af6
Quin Fermstad <fermstq@gmail.com>
2019-01-05 23:04:13 -0800

    Intro Screen
    Loading screen polish and introduction page structure integrated

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,84 @@
+(function() {
+
+	var support = { animations : Modernizr.cssanimations },
+		container = document.getElementById( 'container' ),
+		header = container.querySelector( 'header.header' ),
+		loader = new PathLoader( document.getElementById( 'loader-circle' ) ),
+		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+
+	function init() {
+		var onEndInitialAnimation = function() {
+			if( support.animations ) {
+				this.removeEventListener( animEndEventName, onEndInitialAnimation );
+			}
+
+			startLoading();
+		};
+
+		// disable scrolling
+		window.addEventListener( 'scroll', noscroll );
+
+		// initial animation
+		classie.add( container, 'loading' );
+
+		if( support.animations ) {
+			container.addEventListener( animEndEventName, onEndInitialAnimation );
+		}
+		else {
+			onEndInitialAnimation();
+		}
+	}
+
+	function startLoading() {
+		// simulate loading something..
+		var simulationFn = function(instance) {
+			var progress = 0,
+				interval = setInterval( function() {
+					progress = Math.min( progress + Math.random() * 0.1, 1 );
+
+					instance.setProgress( progress );
+
+					// reached the end
+					if( progress === 1 ) {
+						classie.remove( container, 'loading' );
+						classie.add( container, 'loaded' );
+						clearInterval( interval );
+
+						var onEndHeaderAnimation = function(ev) {
+							if( support.animations ) {
+								if( ev.target !== header ) return;
+								this.removeEventListener( animEndEventName, onEndHeaderAnimation );
+							}
+
+							classie.add( document.body, 'layout-switch' );
+							window.removeEventListener( 'scroll', noscroll );
+						};
+
+						if( support.animations ) {
+							header.addEventListener( animEndEventName, onEndHeaderAnimation );
+						}
+						else {
+							onEndHeaderAnimation();
+						}
+					}
+				}, 80 );
+		};
+
+		loader.setProgressFn( simulationFn );
+	}
+	
+	function noscroll() {
+		window.scrollTo( 0, 0 );
+	}
+
+	init();
+
+})();
+
+
+
 $(document).ready(function() {
     
     $('.project-button').click (function(e) {
@@ -12,6 +93,12 @@ $(document).ready(function() {
             $('#footer').toggleClass ('active');
     }); 
     
+    $('#view-projects').click (function(e) {
+            e.preventDefault();
+            $('#container').toggleClass ('hide');
+            $('#main').toggleClass ('view');
+    }); 
+
     
     
     /* $('#project-button').click (function(e) {
