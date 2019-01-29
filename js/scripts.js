353ff0efc39e582c6769597685cf1d6a7ca24d7e
Quin Fermstad <fermstq@gmail.com>
2019-01-28 20:25:09 -0800

    Mobile project slider

--- js/scripts.js
+++ js/scripts.js
@@ -77,14 +77,13 @@
 
 })();
 
-
-
 $(document).ready(function() {
     
     var childProjectContents = 
     
     $('.project-button').click (function(e) {
         e.preventDefault();
+        $(this).toggleClass ('active');
         $('.project').toggleClass ('active');
         $('.cover').toggleClass ('hide view');
         $('.project-content').toggleClass('hide view');
@@ -96,21 +95,150 @@ $(document).ready(function() {
         $('#footer').toggleClass ('hide view');
     }); 
     
-    $('#intro-wipe').click (function(e) {
+    $('.intro-out').click (function(e) {
         e.preventDefault();
         $('#container').removeClass ('view');
         $('#container').addClass ('hide');
+        $('#main').removeClass ('hide');
         $('#main').addClass ('view');
         $('.loading-complete').addClass ('no-scroll');
     }); 
     
-    $('#about').click (function(e) {
+    $('.intro-in').click (function(e) {
         e.preventDefault();
         $('#container').removeClass ('hide');
         $('#container').addClass ('view');
         $('#main').removeClass ('view');
-        $('#main').addClass ('hide');
         $('.loading-complete').removeClass ('no-scroll');
+        setTimeout(closeMain, 1000);
+    }); 
+    
+    $('#about').click (function(e) {
+        e.preventDefault();
+        $('#about-content').toggleClass ('view');
     }); 
     
 }); 
+
+function closeMain(){
+    $('#main').addClass ('hide');
+}
+
+
+// Project Mobile Slider
+
+// $slider = projectList
+
+// $sliderSlides = project
+
+// $sliderLeft = projectLeft
+
+// $currentSlide = currentProject
+
+// $previousSlide = previousProject
+
+// $slider-slide-hidden = project-hidden
+
+// $slider-slide-active = project-active
+
+// $nextSlide = nextProject
+
+
+(() => {
+    const $projectList = document.querySelector('.project-list');
+
+    const $project = document.querySelectorAll('.project');
+
+    const $projectLeft = document.querySelector('.previous-project');
+
+    const $projectRight = document.querySelector('.next-project');
+
+    // Create method for going on the previous project
+    const projectLeft = (e) => {
+        e.preventDefault();
+
+        // Get current project
+        let $currentProject = document.querySelector('.project-active');
+
+        // Find the previous project
+        let $previousProject = $currentProject.previousElementSibling;
+
+        if ($previousProject !== null) {
+
+            // If we are not on the first project
+            $currentProject.classList.remove('project-active', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $currentProject.classList.add('project-hidden', 'project-slide-out-right');
+
+            $previousProject.classList.remove('project-hidden', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $previousProject.classList.add('project-active', 'project-slide-in-left');
+
+        } else {
+
+            // If we are on the first project
+            $currentProject.classList.remove('project-active', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $currentProject.classList.add('project-hidden', 'project-slide-out-right');
+
+            $projectList.lastElementChild.classList.remove('project-hidden', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $projectList.lastElementChild.classList.add('project-active', 'project-slide-in-left');
+
+        }
+    };
+
+    // Create method for going on the next project
+    const projectRight = (e) => {
+        e.preventDefault();
+
+        // Get current project
+        let $currentProject = document.querySelector('.project-active');
+
+        // Find the next project
+        let $nextProject = $currentProject.nextElementSibling;
+
+        if ($nextProject !== null) {
+            // If we are not on the last project
+            $currentProject.classList.remove('project-active', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $currentProject.classList.add('project-hidden', 'project-slide-out-left');
+
+            $nextProject.classList.remove('project-hidden', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $nextProject.classList.add('project-active', 'project-slide-in-right');
+        } else {
+            // If we are on the last project
+            $currentProject.classList.remove('project-active', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $currentProject.classList.add('project-hidden', 'project-slide-out-left');
+
+            $projectList.firstElementChild.classList.remove('project-hidden', 'project-slide-in-right', 'project-slide-in-left', 'project-slide-out-right', 'project-slide-out-left');
+            $projectList.firstElementChild.classList.add('project-active', 'project-slide-in-right');
+        }
+    };
+
+    // Create new custom event for sliding to the left
+    const projectLeftEvent = new Event('projectLeft');
+
+    // Create new custom event for sliding to the right
+    const projectRightEvent = new Event('projectRight');
+
+    // Create eventListener for click on the left arrow
+    $projectLeft.addEventListener('click', projectLeft, false);
+
+    // Create eventListener for click on the right arrow
+    $projectRight.addEventListener('click', projectRight, false);
+
+    // Create eventListener for pressing the left key
+    $projectLeft.addEventListener('projectLeft', projectLeft, false);
+
+    // Create eventListener for pressing the right key
+    $projectRight.addEventListener('projectRight', projectRight, false);
+
+    // Listen for keydown event
+    document.addEventListener('keydown', (e) => {
+        e = e || window.event;
+
+        if (e.keyCode === 37) {
+            // If pressed key was left arrow
+            $projectLeft.dispatchEvent(projectLeftEvent);
+        } else if (e.keyCode === 39) {
+            // If pressed key was right arrow
+            $projectRight.dispatchEvent(projectRightEvent);
+        }
+    }, false);
+})();
